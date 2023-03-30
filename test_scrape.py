b44c68bf6f3bb4f6577333958bbc3401f0b53389
Dan <dmalengela@yahoo.com>
2023-03-30 13:28:54 +0200

    Removed testing code

--- test_scrape.py
+++ test_scrape.py
@@ -1,12 +1,9 @@
 import unittest
-import Scraper 
-
-class TestScrape(unittest.TestCase):
-
-    def test_scrape(self):
-        self.assertAlmostEqual(Scraper.WriteToCSV(0))
+#import Scraper 
 
 
 
-if __name__== '__main__':
-    unittest.main()
+
+
+
+
